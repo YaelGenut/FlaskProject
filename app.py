d4ec7ed0aea928151d5ad53e7b23cef6756bb361
Yael <yaelgenut24@gmail.com>
2021-12-22 18:56:09 +0200

    hi

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, url_for, render_template
+from flask import Flask, redirect, url_for, render_template, request , session
 
 app = Flask(__name__)
 app.secret_key = "super secret key"
@@ -28,3 +28,46 @@ def maple():
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+
+@app.route('/assignment9', methods=['GET', 'POST'])
+def assignment9_func():
+    users = {'user1': {'name': 'Yael', 'age': '26', 'email': 'yaelgenut@gmail.com'},
+             'user2': {'name': 'Adi', 'age': '19', 'email': 'adi@gmail.com'},
+             'user3': {'name': 'Michal', 'age': '80', 'email': 'Michal@gmail.com'},
+             'user4': {'name': 'Lian', 'age': '5', 'email': 'Lian@gmail.com'},
+             'user5': {'name': 'Niv', 'age': '27', 'email': 'Niv@gmail.com'}}
+
+    if request.method == 'POST':
+        user_nickname= request.form['user_nickname']
+        password= request.form['password']
+        found= True
+        if found :
+            session['user_nickname'] = user_nickname
+            session['user_password'] = password
+            return render_template('assignment9.html')
+        else:
+            return render_template('assignment9.html')
+
+    if request.method == 'GET':
+        if 'user_key' in request.args:
+            if request.args.get('user_key') != '':
+                user_key = request.args['user_key']
+                for key in users:
+                    if key == user_key:
+                        user_name = users[key]['name']
+                        email = users[key]['email']
+                        age = users[key]['age']
+                        return render_template('assignment9.html', u_name=user_name, email=email, age=age
+                                               )
+                return render_template('assignment9.html',not_in='not')
+            return render_template('assignment9.html',  users=users)
+
+        return render_template('assignment9.html')
+
+
+@app.route('/logout')
+def logout_func():
+    session['user_nickname'] = ''
+    session['user_password'] = ''
+    return render_template('assignment9.html')
