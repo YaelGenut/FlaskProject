62866e84a55d077be4850fffc73cab8ac2d46588
Yael <yaelgenut24@gmail.com>
2022-01-04 22:00:00 +0200

    push

---
+++ pages/assignment10/assignment10.py
@@ -0,0 +1,67 @@
+import mysql.connector
+from interact_with_DB import interact_db
+from flask import Blueprint, render_template, redirect, url_for, request, flash
+
+## ‘assignment10’ blueprint
+assignment10 = Blueprint('assignment10', __name__,
+                         static_url_path='/pages/assignment10',
+                         static_folder='static',
+                         template_folder='templates')
+
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="root",
+  password="root"
+)
+
+# Routes
+@assignment10.route('/assignment10', methods=['GET', 'POST'])
+def assignment10_func():
+    query='select * from users'
+    users= interact_db(query=query, query_type='fetch')
+    return render_template('assignment10.html', users= users)
+
+@assignment10.route('/insert_user', methods=['post'])
+def insert_users_func():
+    # get the data
+    name = request.form['name']
+    email = request.form['email']
+    gender = request.form['gender']
+
+    #insert to DB
+    query = "insert into users(name, email, gender) values ('%s','%s','%s'); " % (name, email, gender)
+    interact_db(query=query, query_type='commit')
+
+    # come back to users
+    flash('User inserted successfuly!')
+    return redirect('/assignment10')
+
+
+@assignment10.route('/update_user', methods=['post'])
+def update_users_func():
+    # get the data
+    id = request.form['id']
+    name = request.form['name']
+    email = request.form['email']
+    gender = request.form['gender']
+
+    #update DB
+
+    query = "select id FROM users WHERE id = '%s';" % id
+    query_result = interact_db(query=query, query_type='fetch')
+    if len(query_result) > 0:
+        query = "UPDATE users SET name = '%s', email = '%s' ,gender = '%s' where id='%s' ; " % (name, email, gender ,id)
+        interact_db(query=query, query_type='commit')
+        flash('User updated successfuly!')
+        return redirect('/assignment10')
+    else:
+        flash(f'id user {id} not exist')
+        return redirect('/assignment10')
+
+@assignment10.route('/delete_user', methods=['post'])
+def delete_user_func():
+    user_id= request.form['id']
+    query= "delete from users where id='%s';" % user_id
+    interact_db(query=query, query_type='commit')
+    flash('User deleted successfuly!')
+    return redirect("/assignment10")
---
+++ pages/assignment10/templates/DB_connection.py
@@ -0,0 +1,25 @@
+# in terminal: pip install mysql-connector-python
+import mysql.connector
+
+# ------------- DATABASE CONNECTION --------------- #
+def interact_db(query, query_type: str):
+    return_value = False
+    connection = mysql.connector.connect(host='localhost',
+                                         user='root',
+                                         passwd='root',
+                                         database='myappdb')
+    cursor = connection.cursor(named_tuple=True)
+    cursor.execute(query)
+    #
+
+    if query_type == 'commit':
+        connection.commit()
+        return_value = True
+
+    if query_type == 'fetch':
+        query_result = cursor.fetchall()
+        return_value = query_result
+
+    connection.close()
+    cursor.close()
+    return return_value
